2ce089b1c58ed2472dc3d5754eca85a25aa1c117
Abdon13 <104439971+Abdon13@users.noreply.github.com>
2022-05-31 23:08:12 -0500

    Delete Ej11_1.js

--- Ej11_1.js
+++
@@ -1,4 +0,0 @@
-let animales = ["Perro", "Gato", "Periquito", "Conejo"];
-let equipos = new Array ("FCBarcelona", "Atletico de Madrid", "Real Madrid");
-console.log(animales);
-console.log(equipos);
