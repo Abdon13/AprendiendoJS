d222fd81e26b4c7304368877e8e3a6ba569f5f5d
Abdon13 <104439971+Abdon13@users.noreply.github.com>
2022-05-31 23:08:50 -0500

    Delete Ej11_6.js

--- Ej11_6.js
+++
@@ -1,7 +0,0 @@
-let animales = ["Perro", "Gato", "Periquito", "Conejo"];
-console.log(animales);
-let eliminado = animales.shift();
-console.log("Elemento eliminado: ", eliminado);
-console.log(animales);
-animales.unshift("Caballo");
-console.log(animales);
